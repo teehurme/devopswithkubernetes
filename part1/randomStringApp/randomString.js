536cdc12a52515dc7ea90f718c943a626beae044
teehurme <1544507+teehurme@users.noreply.github.com>
2022-06-16 00:02:41 +0300

    Exercise 1.07

--- part1/randomStringApp/randomString.js
+++ part1/randomStringApp/randomString.js
@@ -1,7 +1,20 @@
 import { randomUUID } from 'node:crypto'
-const randomString = randomUUID()
+import express from 'express'
 
+
+const randomString = randomUUID()
+const app = express();
+const PORT = process.env.PORT ?? '8000'
+
+app.get('/',(req,res)=>{
+    const time= new Date()
+    res.send(`${time.toISOString()}: ${randomString}`);
+})
 setInterval(function(){
     const time= new Date()
     console.log(time.toISOString(),': ',randomString)
 },5000);
+
+app.listen(PORT,()=>{
+    console.log(`server listening on port ${PORT}`)
+})
