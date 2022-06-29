b4c112e86780504b348cc22368bea78d3770978a
teehurme <1544507+teehurme@users.noreply.github.com>
2022-06-30 00:32:10 +0300

    fix Exercise 2.01: Connecting pods

--- part1/log_output/reader/reader.js
+++ part1/log_output/reader/reader.js
@@ -10,7 +10,7 @@ const FILEPATH=process.env.FILEPATH ?? './files/sharedfile'
 app.get('/',async (req,res)=>{
     const data = (await fs.readFile(FILEPATH)).toString()
    // const pongsData = (await fs.readFile(PONGFILEPATH).catch((err)=>console.log(err)))
-    const result = await axios.get('http://pong-svc/count');
+    const result = await axios.get('http://pong-svc:1234/count');
     res.send(`${data}</br>PING / PONGS: ${result.data}`);
 });
 app.listen(PORT,()=>{
