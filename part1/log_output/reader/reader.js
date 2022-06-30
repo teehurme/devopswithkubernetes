c88b6f38751f5b1191747d814d68f17f207d3b15
teehurme <1544507+teehurme@users.noreply.github.com>
2022-06-30 21:05:45 +0300

    Exercise 2.06: Documentation and ConfigMaps

--- part1/log_output/reader/reader.js
+++ part1/log_output/reader/reader.js
@@ -4,14 +4,14 @@ import axios from 'axios'
 const app = express();
 const PORT = process.env.PORT ?? '8000'
 const FILEPATH=process.env.FILEPATH ?? './files/sharedfile'
-
+const MESSAGE = process.env.MESSAGE ?? 'no message'
 
 
 app.get('/',async (req,res)=>{
     const data = (await fs.readFile(FILEPATH)).toString()
    // const pongsData = (await fs.readFile(PONGFILEPATH).catch((err)=>console.log(err)))
     const result = await axios.get('http://pong-svc:1234/count');
-    res.send(`${data}</br>PING / PONGS: ${result.data}`);
+    res.send(`${MESSAGE}</br>${data}</br>PING / PONGS: ${result.data}`);
 });
 app.listen(PORT,()=>{
     console.log(`server listening on port ${PORT}`)
